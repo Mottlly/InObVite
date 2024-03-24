a6eabb92be53cb178721c3309367e9bb182dcf1b
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-03-24 15:29:16 -0300

    created the opening crawl, fuhn!

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import OpeningCrawl from './anothercomp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
+    <OpeningCrawl />
   </React.StrictMode>
 )
